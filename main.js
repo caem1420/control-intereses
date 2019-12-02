9526d2c75e68aa6d683383530dba63b14d835cc7
Carlos Escobar <ecarlosandres@gmail.com>
2019-12-02 11:07:15 -0500

    primero

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var express = require('express');
+// in sublime
+var express = require(‘express’);
+var port = process.env.PORT || 3000;
 var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get(‘/’, function (req, res) {
+ res.send(JSON.stringify({ Hello: ‘World’}));
 });
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+ console.log(`Example app listening on port !`);
 });
