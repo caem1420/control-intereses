fbc3725ba9ca08f00715bd088c98056b68e9e190
Carlos Escobar <ecarlosandres@gmail.com>
2019-12-02 11:16:09 -0500

    primero11

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 app.get('/', function (req, res) {
- res.sendFile("./index.html");
+ res.sendFile(__dirname  +"/index.html");
 });
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
